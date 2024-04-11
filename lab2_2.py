578105c03b8c524e563f74b9ec155eaf0d5014e7
Pavlo.Levchenko <Pavlo.Levchenko@gmail.com>
2024-04-11 21:25:00 +0300

    pract2 commit10

--- lab2_2.py
+++ lab2_2.py
@@ -8,17 +8,27 @@ class History:
         if action.startswith('url:'):
             self.stack.append(action)
             self.stack_forward = []
+            print(f'{self.stack=}')
+            print(f'{self.stack_forward=}')
         elif action == 'back':
             self.stack_forward.append(self.stack.pop())
+            print(f'{self.stack=}')
+            print(f'{self.stack_forward=}')
         elif action == 'forward':
             self.stack.append(self.stack_forward.pop())
+            print(f'{self.stack=}')
+            print(f'{self.stack_forward=}')
 
 
 actions = ['url:', 'back', 'forward']
 history1 = History()
 history1.action('url:ukr.net')
 history1.action('url:ukr.net/mail')
-
+history1.action('url:pravda.com.ua')
+history1.action('back')
+history1.action('back')
+history1.action('forward')
+history1.action('url:google.com.ua')
 # base action
 # new url
 # back
