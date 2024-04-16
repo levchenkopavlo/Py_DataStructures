bb23aa50f24639f6efbbf830db90e2c6dbcb9d83
Pavlo.Levchenko <Pavlo.Levchenko@gmail.com>
2024-04-16 15:42:05 +0300

    hw2 1-2 commit 1

--- hw2_2.py
+++ hw2_2.py
@@ -1,28 +1,18 @@
-# Завдання 1
-# Реалізуйте клас стеку для роботи з рядками (стек рядків).
-# Стек має бути фіксованого розміру. Реалізуйте набір операцій
-# для роботи зі стеком:
-# o розміщення рядка у стек;
-# o виштовхування рядка зі стеку;
-# o підрахунок кількості рядків у стеку;
-# o перевірку, чи порожній стек;
-# o перевірку, чи повний стек;
-# o очищення стеку;
-# o отримання значення без виштовхування
-# верхнього рядка зі стеку.
-# На старті додатка відобразіть меню, в якому користувач
-# може вибрати необхідну операцію.
+# Завдання 2
+# Змініть стек із першого завдання таким чином, щоб його
+# розмір був нефіксованим.
 class Stack:
-    def __init__(self, capacity=5):
+    def __init__(self):
         self._stack = []
-        self._capacity = capacity
 
-    def insert(self, item):
-        if len(self._stack) < self._capacity:
-            self._stack.insert(0, item)
+    def insert(self, new_item):
+        self._stack.insert(0, new_item)
+
+    def pop_item(self):
+        if not self._stack:
+            print("stack is empty")
             return None
         else:
-            self._stack.insert(0, item)
             return self._stack.pop()
 
     def get_item(self):
@@ -38,12 +28,6 @@ class Stack:
     def is_empty(self):
         return bool(not self._stack)
 
-    def is_full(self):
-        if len(self._stack) == self._capacity:
-            return True
-        else:
-            return False
-
     def clean(self):
         self._stack = []
 
@@ -58,7 +42,7 @@ try:
         print(f"\033[33m2\033[0m. Виштовхнути рядок з стеку")
         print(f"\033[33m3\033[0m. Кількість елементів стеку")
         print(f"\033[33m4\033[0m. Стек порожній?")
-        print(f"\033[33m5\033[0m. Стек повний?")
+        # print(f"\033[33m5\033[0m. Стек повний?")
         print(f"\033[33m6\033[0m. Очистити стек")
         print(f"\033[33m7\033[0m. Отримати значення без виштовхування")
         print(f"\033[33m8\033[0m. Переглянути вміст стеку")
@@ -71,15 +55,14 @@ try:
                 print("Введіть рядок для додавання в стек: ")
                 string_for_stack = input()
                 stack1.insert(string_for_stack)
-
             case "2":
-                print(stack1.insert(None))
+                print(stack1.pop_item())
             case "3":
                 print(stack1.get_len())
             case "4":
                 print(stack1.is_empty())
-            case "5":
-                print(stack1.is_full())
+            # case "5":
+            #     print(stack1.is_full())
             case "6":
                 stack1.clean()
             case "7":
