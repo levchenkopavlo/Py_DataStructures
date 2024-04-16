087ea2c10832eee84dca1fd2949a1d084268d064
Pavlo.Levchenko <Pavlo.Levchenko@gmail.com>
2024-04-16 18:29:00 +0300

    hw2-2 1-2 commit 1

--- hw2_3.py
+++ hw2_3.py
@@ -9,21 +9,86 @@
 # рекурсивного алгоритму, використовуючи стек для
 # зберігання проміжних ходів при переміщенні дисків між
 # вежами
-def move_disk(s1, s2, s3):
-    if len(s2) == 0:
-        s2.append(s1.pop())
-    else:
-        if s1[-1] > s2[-1]:
-            s2.append(s1.pop())
-    move_disk()
+def move_disk(*args):
+    last_element = args[0]
+    stack1 = args[1]
+    stack2 = args[2]
+    stack3 = args[3]
+    counter = args[4]
+    print(counter)
+    if len(stack1) == num_disk and last_element != 0:
+        return
+    if len(stack3) == num_disk:
+        print(stack3)
+        print(counter)
+        quit()
+    if stack1:  # стек1 не порожній
+        if stack1[-1] != last_element:
+            if stack2:  # стек2 не порожній
+                if stack1[-1] < stack2[-1]:
+                    stack2.append(stack1.pop())
+                    move_disk(stack2[-1], stack1, stack2, stack3, counter+1)
+            else:
+                stack2.append(stack1.pop())
+                move_disk(stack2[-1], stack1, stack2, stack3, counter+1)
+
+    if stack1:  # стек1 не порожній
+        if stack1[-1] != last_element:
+            if stack3:  # стек3 не порожній
+                if stack1[-1] < stack3[-1]:
+                    stack3.append(stack1.pop())
+                    move_disk(stack3[-1], stack1, stack2, stack3, counter+1)
+            else:
+                stack3.append(stack1.pop())
+                move_disk(stack3[-1], stack1, stack2, stack3, counter+1)
+    # ====================================================================================
+    if stack2:  # стек2 не порожній
+        if stack2[-1] != last_element:
+            if stack1:  # стек1 не порожній
+                if stack2[-1] < stack1[-1]:
+                    stack1.append(stack2.pop())
+                    move_disk(stack1[-1], stack1, stack2, stack3, counter+1)
+            else:
+                stack1.append(stack2.pop())
+                move_disk(stack1[-1], stack1, stack2, stack3, counter+1)
+
+    if stack2:  # стек2 не порожній
+        if stack2[-1] != last_element:
+            if stack3:  # стек3 не порожній
+                if stack2[-1] < stack3[-1]:
+                    stack3.append(stack2.pop())
+                    move_disk(stack3[-1], stack1, stack2, stack3, counter+1)
+            else:
+                stack3.append(stack2.pop())
+                move_disk(stack3[-1], stack1, stack2, stack3, counter+1)
+    # ====================================================================================
+    if stack3:  # стек3 не порожній
+        if stack3[-1] != last_element:
+            if stack1:  # стек1 не порожній
+                if stack3[-1] < stack1[-1]:
+                    stack1.append(stack3.pop())
+                    move_disk(stack1[-1], stack1, stack2, stack3, counter+1)
+            else:
+                stack1.append(stack3.pop())
+                move_disk(stack1[-1], stack1, stack2, stack3, counter+1)
+
+    if stack3:  # стек3 не порожній
+        if stack3[-1] != last_element:
+            if stack2:  # стек2 не порожній
+                if stack3[-1] < stack2[-1]:
+                    stack2.append(stack3.pop())
+                    move_disk(stack2[-1], stack1, stack2, stack3, counter+1)
+            else:
+                stack2.append(stack3.pop())
+                move_disk(stack2[-1], stack1, stack2, stack3, counter+1)
+    return
 
 
 stack1 = []
 stack2 = []
 stack3 = []
-num_disk = 3
-for i in range(num_disk):
-    stack1.append(i + 1)
+num_disk = 2
+for i in range(num_disk, 0, -1):
+    stack1.append(i)
 print(stack1)
-move_disk(stack1, stack2, stack3)
-# print(len(stack2)>0 or stack1[-1]>stack2[-1])
+move_disk(0, stack1, stack2, stack3, 1)
