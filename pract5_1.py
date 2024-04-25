5add2b654170174325acca00182c82b0da95b072
Pavlo.Levchenko <Pavlo.Levchenko@gmail.com>
2024-04-25 21:09:09 +0300

    pr6 JSON commit2

--- pract5_1.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-data = {'login': 'fdghfg', 'password': 'dfgdf1112'}
-serialized_data = pickle.dumps(data)
-print(serialized_data)
-print(pickle.loads(serialized_data))
-
--- temp.py
+++
@@ -1,7 +0,0 @@
-import pickle, gzip
-
-with open('person.pickle', 'rb') as file:
-    read_person = pickle.load(file)
-
-print((type(read_person)), read_person)
-# read_person.print_info()
