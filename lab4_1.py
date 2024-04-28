7ced7e8f8c88097aa64635ff00b9f632f0146e93
Pavlo.Levchenko <Pavlo.Levchenko@gmail.com>
2024-04-28 16:47:16 +0300

    lab4 Tree commit4

--- lab4_1.py
+++ lab4_1.py
@@ -10,151 +10,106 @@
 # ■ відображати топ-10 найнепопулярніших слів (визначаємо непопулярність спираючись на лічильник
 # звернень).
 # Використовуйте дерево для виконання цього завдання.
+# Завдання 1
+# Створіть програму роботи зі словником.
+# Наприклад, англо-іспанський, французько-німецький або інша мовна пара.
+# Програма має:
+# ■ надавати початкове введення даних для словника;
+# ■ відображати слово та його переклади;
+# ■ дозволяти додавати, змінювати, видаляти переклади слова;
+# ■ дозволяти додавати, змінювати, видаляти слово;
+# ■ відображати топ-10 найпопулярніших слів (визначаємо популярність спираючись на лічильник звернень);
+# ■ відображати топ-10 найнепопулярніших слів (визначаємо непопулярність спираючись на лічильник
+# звернень).
+# Використовуйте дерево для виконання цього завдання.
 from bintrees import AVLTree
-import random
-
-
-def generate_id():
-    return float(random.randint(1, 999999) / 1000000)
-
-
-class Word:
-    def __init__(self, word):
-        self.word = word
-        self.eng_translate = {}
-        self.call_counter = generate_id()
-
-    def __str__(self):
-        return f'{self.word}: {self.eng_translate}, {self.call_counter}'
 
 
 class Dictionary:
     def __init__(self):
         self.tree = AVLTree()
+        self.call_counter = {}
 
-    def add_word(self, new_word, translate):
-        word_for_tree = Word(new_word)
-        for word in self.tree.values:
-            if new_word == word.word:
-                pass
-            else:
-                self.tree.insert(key=word_for_tree.call_counter, value=word_for_tree)
+    def show(self, word):
+        if word in self.tree:
+            self.call_counter[word] += 1
+            return f'{word}: {self.tree[word]}'
+        else:
+            return None
 
-    def search(self, word):
-        word = Word(word)
+    def show_all(self):
+        for word in self.tree:
+            print(f'{word}: {self.tree[word]}')
 
-        # if word in self.dictionary:
-        #     return True
-        # else:
-        #     print('word is not in the dictionary')
+    def add_word(self, new_word, *translate):
+        if new_word in self.tree:
+            self.tree[new_word] |= set(translate)
+        else:
+            self.tree.insert(key=new_word, value=set(translate))
+            self.call_counter[new_word] = 0
 
-    # ------------------------------------------------------------------------------------------------------
-    def add_person(self, person):
-        if self.search_id(person.id):
+    def edit_word(self, word, new_word):
+        if word in self.tree:
+            self.add_word(new_word, *self.tree[word])
+            self.tree.remove(word)
+        else:
+            print('no record in base')
+
+    def edit_translate(self, word, *translate):
+        if word in self.tree:
+            self.tree[word] = set(translate)
+        else:
+            self.tree.insert(key=word, value=set(translate))
+
+    def remove_word(self, word):
+        if word in self.tree:
+            self.tree.remove(word)
+        else:
             pass
-        else:
-            self.tree.insert(key=person.id, value=person)
 
-    def penalty_add(self, person_id, penalty):
-        if self.search_id(person_id):
-            person = self.tree[person_id]
-            self.tree.remove(person_id)
-            if person.penalty is not None:
-                for penalty_type, value in penalty:
-                    if penalty_type not in person.penalty:
-                        person.penalty[penalty_type] = value
-                    else:
-                        person.penalty[penalty_type].append(value)
-            else:
-                person.penalty = penalty
-            self.add_person(person)
-        else:
-            print('person not exist')
-
-    def penalty_edit(self, person_id, penalty):
-        if self.search_id(person_id):
-            person = self.tree[person_id]
-            self.tree.remove(person_id)
-            person.penalty = penalty
-            self.add_person(person)
-        else:
-            print('person not exist')
-
-    def search_id(self, person_id):
-        if person_id in self.tree:
+    def search(self, search_word):
+        if search_word in self.tree:
             return True
         else:
             return False
 
-    def delete(self, person_id):
-        if person_id in self.tree:
-            self.tree.remove(person_id)
+    def show_statistic(self):
+        print(f'статистика запитів: {self.call_counter}')
+
+    def get_statistic(self):
+        return {self.call_counter}
+
+    def show_most_usable(self, number=0):
+        sorted_dict_values = sorted(self.call_counter.items(), key=lambda x: x[1], reverse=True)
+        if number:
+            print(*sorted_dict_values[0:number])
         else:
-            print('no record')
-
-    def display(self):
-        for person in self.tree.values():
-            print(person)
-
-    def filter_id(self, person_id):
-        for person in self.tree.values():
-            if person_id == person.id:
-                print(person)
-
-    def filter_city(self, city):
-        for person in self.tree.values():
-            if city == person.city:
-                print(person)
-
-    def filter_city(self, city):
-        for person in self.tree.values():
-            if city == person.city:
-                print(person)
-
-    def filter_penalty_type(self, penalty_type):
-        for person in self.tree.values():
-            if person.penalty is not None:
-                if penalty_type in person.penalty:
-                    print(person)
+            print(*sorted_dict_values)
 
 
 # ------------------------------------------------------------------------------------------------------
 dictionary = Dictionary()
-dictionary.add_word('запит', None)
-dictionary.search('запит')
+dictionary.add_word('запит', 'request')
+print(dictionary.search('запит'))
+print(dictionary.show('запит'))
+dictionary.add_word('запит', 'question')
+print(dictionary.show('запит'))
+dictionary.add_word('стіл', 'table')
+print(dictionary.show('стіл'))
+dictionary.add_word('кран', 'crane', 'tap')
+print()
+dictionary.show_all()
+dictionary.edit_translate('запит', 'request')
+print(dictionary.show('кран'))
+print(dictionary.show('кран'))
+print(dictionary.show('кран'))
+print()
+dictionary.show_all()
+print()
+dictionary.show_statistic()
+dictionary.edit_word('стіл', 'столище')
+dictionary.show_all()
 
 print()
-
-
-# ------------------------------------------------------------------------------------------------------
-class Person:
-    def __init__(self, name, city, penalty=None):
-        self.id = str(generate_id())
-        self.penalty = penalty
-        self.city = city
-        self.name = name
-
-    def __call__(self, *args, **kwargs):
-        pass
-
-    def __str__(self):
-        return f'name: {self.name}, id: {self.id}, city: {self.city}, penalty: {self.penalty}'
-
-# person1 = Person("Bob Peterson", 'NY', {'1': [100, 120], '2': [200]})
-# person2 = Person("Петрик Пяточкін", 'Київ')
-# person3 = Person("Івасик Телесик", 'Полтава')
-# print(person1)
-# print(person2)
-# penalty_base = PenaltyTree()
-# penalty_base.add_person(person1)
-# print(penalty_base.search_id(person1.id))
-# print(penalty_base.search_id(person2.id))
-# penalty_base.add_person(person2)
-# print()
-# penalty_base.display()
-# print()
-# penalty_base.filter_id(input('enter id: '))
-# print()
-# penalty_base.filter_penalty_type('1')
-# print()
-# penalty_base.penalty_add(input('enter id: '))
+dictionary.show_most_usable()
+dictionary.show_most_usable(2)
