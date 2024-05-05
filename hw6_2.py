2f505b66a379a63101940deb3dcd1d4e703ba359
levchenkopavlo <levchenkopavlo@gmail.com>
2024-05-06 00:10:35 +0300

    hw6 JSON commit5

--- hw6_2.py
+++ hw6_2.py
@@ -30,20 +30,23 @@ class Client():
             json.dump(data, file)
             file.write('\n')
 
-    def load_data(self):
+    @classmethod
+    def load_data(cls):
         with open(f'survey.json', 'r', encoding="utf-8") as file:
             data = file.readlines()
-
-        # print(data)
-        # data = {}
-        # for key in self.__dict__:
-        #     data[key] = self.__dict__[key]
-        # with open(f'survey.json', 'a', encoding="utf-8") as file:
-        #     json.dump(data, file)
-        #     file.write('\n')
+            for line in data:
+                survey = json.loads(line.strip())
+                # print(survey)
+                print(f"Client: {survey['_name']}, age: {survey['_age']}")
+                print(f'Questions and answers')
+                for item in list(zip(survey['_question_list'], survey['_answer_list'])):
+                    print(f'{item[0]}: {item[1]}')
 
 
 question_list = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6']
 user1 = Client('Steven', 29, question_list)
-user1.survey()
-user1.load_data()
+# user1.survey()
+# user2 = Client('Bob', 30, question_list)
+# user2.survey()
+
+Client.load_data()
